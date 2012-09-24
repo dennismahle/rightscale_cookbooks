d190ca791afed6177a03607d47afa565e09cb05c
dennismahle <dennis@watchwith.com>
2012-09-24 23:16:57 +0000

    Touched metadata.rb

--- cookbooks/app_tomcat/metadata.rb
+++ cookbooks/app_tomcat/metadata.rb
@@ -1,9 +1,9 @@
-maintainer       "RightScale, Inc."
+maintainer       "Watchwith, Inc."
 maintainer_email "support@rightscale.com"
 license          "Copyright RightScale, Inc. All rights reserved."
 description      "Installs the tomcat application server."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "12.1.0"
+version          "12.1.1"
 
 supports "centos", "~> 5.8"
 supports "redhat", "~> 5.8"
